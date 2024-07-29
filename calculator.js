c2b0b8602aa2afcea751b6fe539aa42c6044504b
John <141792618+michaellucas2008@users.noreply.github.com>
2024-07-29 00:25:51 -0400

    add event listener to the div containing the buttons. it only display the text of the button that was pressed to a div

--- calculator.js
+++ calculator.js
@@ -10,4 +10,49 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
-
+function reverseNumberSign(text){
+    const array=[...text];
+    if(text[0]==="-"){
+        array.splice(0,1);
+    }
+    else{
+        array.unshift("- ");
+    }
+    return array.join('');
+}
+const display= document.querySelector(".container-top-display");
+const bottomContainer=document.querySelector(".container-bottom");
+bottomContainer.addEventListener("click",(event)=>{
+    event.stopPropagation();
+    if(event.target.className !== "container-bottom"){
+        const buttonString = event.target.innerText;
+        if(Number.isInteger(Number(buttonString))){
+            display.textContent='';
+            display.textContent=`${buttonString}`;
+        }
+        else{
+            switch(buttonString){
+                case "+":
+                    break; 
+                case "-":
+                    break;  
+                case "AC":
+                    display.textContent='0';
+                    break;
+                case "÷":
+                    break;
+                case "×":
+                    break;
+                case "%":
+                    break;
+                case "=":
+                    break;
+                case "-/+":
+                    display.textContent=`${reverseNumberSign(display.textContent)}`
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    },false);
